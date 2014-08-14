0eab134376deb9031640ac0b10e97d607c41f850
oliver@xlearnlab.net <oliver@xlearnlab.net>
2014-08-14 10:53:27 -0400

    Initial release

---
+++ js/adapt-xlearnlab-boxlinks.js
@@ -0,0 +1,40 @@
+/*
+* adapt-xlearnlab-boxlinks
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Oliver Pincus <oliver.pincus@xlearnlab.net>
+*/
+define(function(require) {
+  var ComponentView = require('coreViews/componentView');
+  var Adapt = require('coreJS/adapt');
+
+  var Boxlinks = ComponentView.extend({
+
+    postRender: function() {
+      this.setReadyStatus();
+      this.$('.component-inner').on('inview', _.bind(this.inview, this));
+    },
+
+    inview: function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            if (visiblePartY === 'top') {
+                this._isVisibleTop = true;
+            } else if (visiblePartY === 'bottom') {
+                this._isVisibleBottom = true;
+            } else {
+                this._isVisibleTop = true;
+                this._isVisibleBottom = true;
+            }
+
+            if (this._isVisibleTop && this._isVisibleBottom) {
+                this.$('.component-inner').off('inview');
+                this.setCompletionStatus();
+            }
+            
+        }
+    }
+
+  });
+
+  Adapt.register("boxlinks", Boxlinks);
+
+});
